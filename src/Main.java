8005fd9de6024d234cb1126b7b3f8742830b58f5
Ekaterina <katieleyvi@gmail.com>
2023-05-18 11:28:46 -0700

    added for loop from 0 to 100

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        for(int i = 0; i <= 100; i++){
+            System.out.println(i);
+        }
     }
 }
