07162ffa122bcd71cc41a987e1e1ce9dadcdc50c
Ekaterina <katieleyvi@gmail.com>
2023-05-18 12:00:47 -0700

    new string append

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         for(int i = 0; i <= 200; i++){
-            System.out.println(i);
+            System.out.printf("the number is: %d\n", i);
         }
     }
 }
