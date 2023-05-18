afaecd21c04e0963c5ecf7cc657c7eeb5d25552a
Ekaterina <katieleyvi@gmail.com>
2023-05-18 11:42:13 -0700

    person

---
+++ src/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    
+    public void greeting() {
+        System.out.printf("hello, my name is %s, im %d years old", name, age);
+    }
+}
