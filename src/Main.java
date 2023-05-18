827a507df0972a7d6930965b789061ffc6454dc9
Ekaterina <katieleyvi@gmail.com>
2023-05-18 12:02:10 -0700

    new string append

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@
 public class Main {
     public static void main(String[] args) {
         for(int i = 0; i <= 200; i++){
-            System.out.println(i);
+            for(int j = 0; j <= 200; j++){
+                System.out.println(i*j);
+            }
         }
     }
 }
